64d43f6a983db14b4ac2eaec08f88ac7043e4c37
ArnauCodes <57684378+ArnauCodes@users.noreply.github.com>
2020-10-12 21:17:39 +0100

    Update CompletingTheSquareSolver.py

--- projects/CompletingTheSquareSolver.py
+++ projects/CompletingTheSquareSolver.py
@@ -1,5 +1,4 @@
-# Solve quadratic equations by Completing the square
-# Solve quadratic equations by Completing the square
+# Programme to solve quadratic equations by Completing the square
 # The input must only contain a, b, c where a, b, c are real numbers. It is also supposed that the equation's form is of: ax^2 + bx + c = 0
 # -1 is also returned for non-real answers not involving the complex world
 from math import sqrt
