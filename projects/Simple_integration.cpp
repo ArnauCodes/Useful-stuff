e59853572768a595b8b8fce45ce146434d26c4bc
ArnauCodes <57684378+ArnauCodes@users.noreply.github.com>
2021-02-17 15:47:33 +0100

    Update Simple_integration.cpp

--- projects/Simple_integration.cpp
+++ projects/Simple_integration.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 #include <string>
 
-std::string solve(int *exp, std::string *variable, std::string *e_ans, std::string *e_den){
-  int dd = std::int_fast16_t((*exp)++);
-  int exp_pp = std::int_fast16_t((*exp));
-  return (*exp) == 0 ? "ln|x| +c" :   
-(("(("+*variable)+")" + (*e_ans) + std::to_string(*exp))+")" + (*e_den) + std::to_string(*exp) + " +c";
+// Exponent must be an int
+std::string solve(int *exp, std::string *variable, std::string *e_ans, double *coeff){
+  double nume = (*coeff); 
+  (*exp)++;
+  return ((*exp) == -1) ? "ln|x| +c" : (std::to_string(nume / (*exp)) + (*variable) + (*e_ans) + std::to_string((*exp)) + ") +c");
 };
 
 int main() {
   int exp;
-  std::string variable, c_str = "Integrate with respect to: " + variable, e_ans = "^", e_den = "/";
+  double coeff;
+  std::string variable, c_str = "Integrate with respect to: " + variable, e_ans = "^(";
   std::cout << c_str;
   std::cin >> variable;
   std::cout << "Exponent of " + variable + " = ";
   std::cin >> exp;
-  std::cout << solve(&exp, &variable, &e_ans, &e_den) << std::endl;
+  std::cout << "Coeffiecent of " + variable + " = ";
+  std::cin >> coeff;
+  std::cout << solve(&exp, &variable, &e_ans, &coeff) << std::endl;
   return 0;
 };
